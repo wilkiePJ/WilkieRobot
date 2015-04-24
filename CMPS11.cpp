0f103625d9e5a5d796f3534aec224c99827edd30
wilkiePJ <philipwilkins@sky.com>
2015-04-24 22:07:05 +0000

    added CMPS11 driver

---
+++ CMPS11.cpp
@@ -0,0 +1,219 @@
+// CMPS11.cpp - Controller functions for the compass module
+// Author: Phil Wilkins
+// Create Date: 24/04/2015
+// Version: 0.1
+// Dependancies : BCM2835.h - clone from git @ https://github.com/wilkiePJ/BCM2835.git
+
+#include <iostream>
+#include "CMPS11.h"
+
+
+//FUNCTION showSoftVersion
+//PURPOSE show the current software version of the hardware.
+void Orientiation::showSoftVersion()
+{
+	int softVersion=0;
+	
+	softVersion=readReg8(COMMAND_REG);
+	if(softVersion==-1)
+	{
+		std::cout << "CMPS11 ERROR: Could not retrieve the software version" << std::endl;
+		return;
+	}
+	else
+	{
+		std::cout << "CMPS11 Orientation Module Software version: " << softVersion << std::endl;
+		return;
+	}
+
+}
+
+//FUNCTION calibrateDevice
+//PURPOSE Enter calibration mode
+int Orientation::calibrateDevice()
+{
+return(0);
+}
+
+//FUNCTION: restoreFactoryDefault
+//PURPOSE reset the device to factory default settings.
+int Orientation::restoreFactoryDefault()
+{
+return(0);
+}
+	
+//FUNCTION: m_ getBearing16 
+//PURPOSE read the compass data, returns a short. returns -1 on error, otherwise a compass bearing between 0 and 360 degrees
+short Orientation::m_getBearing16()
+{
+	int err[2]={0,0};
+	int8 high=0;
+	short result=0;
+	
+
+	//read compass angle 
+	for(int i=0; i<2; i++)
+	{
+		err[i]=readReg8(i+COMPASS_16_HIGH);
+		if(err[i]==-1)
+		{
+			result=-1;
+		}
+		else
+		{
+			if(i==0) high=(err[i] & 0xFF); else result=(short)((high <<8) | (err[i] & 0xFF)); 
+		}
+		usleep(1000);
+	}
+	
+	return(result);
+	
+}
+ 
+//FUNCTION: m_getBearing8 
+//PURPOSE read the compass data, returns a short. -1 on fail otherwise from 0 - 255 representing a full circle
+short Orientation::m_getBearing8()
+{
+	int err=0;
+
+	err=readReg8(COMPASS_8);
+	if(err=-1) return(-1) else return(err);	
+}
+
+
+//FUNCTION getMagnetometer
+//PURPOSE read the Magnetometer data, returns a structure - returns -1 on each coordinate if failes to read.
+struct m_coord Orientaion::getMagnometer()
+{
+	int err[6]={0,0,0,0,0,0};
+	int8 high=0;
+	struct m_coord result;
+	
+
+	//read coordinates
+	for(int i=0; i<6; i++)
+	{
+		err[i]=readReg8(i+MAGNETOMETER_X_RAW_HIGH);
+		if(err[i]==-1)
+		{
+			result.x=-1;
+			result.y=-1;
+			result.z=-1;
+		}
+		else
+		{
+			if(i==0) high=(err[i] & 0xFF); else result.x=(short)((high <<8) | (err[i] & 0xFF)); 
+			if(i==2) high=(err[i] & 0xFF); else result.y=(short)((high <<8) | (err[i] & 0xFF));
+			if(i==4) high=(err[i] & 0xFF); else result.z=(short)((high <<8) | (err[i] & 0xFF)); 
+		}
+		usleep(1000);
+	}
+	
+	return(result);
+}
+
+
+//FUNCTION getGyro
+//PURPOSE read the Gyro data, returns a structure - returns -1 on each coordinate if failes to read.
+struct m_coord Orientation::getGyro()
+{
+	int err[6]={0,0,0,0,0,0};
+	int8 high=0;
+	struct m_coord result;
+	
+
+	//read coordinates
+	for(int i=0; i<6; i++)
+	{
+		err[i]=readReg8(i+GYRO_X_RAW_HIGH);
+		if(err[i]==-1)
+		{
+			result.x=-1;
+			result.y=-1;
+			result.z=-1;
+		}
+		else
+		{
+			if(i==0) high=(err[i] & 0xFF); else result.x=(short)((high <<8) | (err[i] & 0xFF)); 
+			if(i==2) high=(err[i] & 0xFF); else result.y=(short)((high <<8) | (err[i] & 0xFF));
+			if(i==4) high=(err[i] & 0xFF); else result.z=(short)((high <<8) | (err[i] & 0xFF)); 
+		}
+		usleep(1000);
+	}
+	
+	return(result);
+}
+
+//FUNCTION getAccelerometer
+//PURPOSE read the acceleromter data, returns a structure - returns -1 on each coordinate if failes to read.
+struct m_coord Orientation::getAccelerometer()
+{
+	int err[6]={0,0,0,0,0,0};
+	int8 high=0;
+	struct m_coord result;
+
+
+	//read coordinates
+	for(int i=0; i<6; i++)
+	{
+		err[i]=readReg8(i+ACCELEROMETER_X_RAW_HIGH);
+		if(err[i]==-1)
+		{
+			result.x=-1;
+			result.y=-1;
+			result.z=-1;
+		}
+		else
+		{
+			if(i==0) high=(err[i] & 0xFF); else result.x=(short)((high <<8) | (err[i] & 0xFF)); 
+			if(i==2) high=(err[i] & 0xFF); else result.y=(short)((high <<8) | (err[i] & 0xFF));
+			if(i==4) high=(err[i] & 0xFF); else result.z=(short)((high <<8) | (err[i] & 0xFF)); 
+		}
+		usleep(1000);
+	}
+	
+	return(result);
+}
+
+int8 Orientation::getPitch(bool mode)
+{		// true=filter on, false=filter off
+	int err=0;
+
+	switch(mode)
+	{
+		case true: break;
+		case false: break;
+		default: break;
+	}
+}
+
+int8 Orientation::getRoll(bool mode)
+{
+	int err=0;
+
+	switch(mode)
+	{
+		case true: break;
+		case false: break;
+		default: break;
+	}
+
+}
+
+short Orientation::convertShort(int8 high, int8 low)
+{
+	return((high << 8) | low);
+}
+
+Orientation::Orientation()
+{
+	//setBaud(90000);
+	setSlaveAddr(CMPS11_ADDR); //set up the I2C address for the module
+	configureDevice(I2C_BASE_ADDR);
+}
+
+Orientation::~Orientation()
+{
+	closeDevice();
+}
+
---
+++ CMPS11.h
@@ -0,0 +1,79 @@
+// CMPS11 Driver class - 
+// Author: Phil Wilkins 
+// Create Date: April 24 2015 
+// Version: 0.1
+// Dependancies : BCM2835.h - clone from git @ https://github.com/wilkiePJ/BCM2835.git
+
+#ifndef CMPS11_H
+#define CMPS11_H
+
+#include "BCM2835.h"
+#include <sys/types.h>
+#include <unistd.h>
+
+// Register layout for the CMPS11 in I2C mode.
+
+#define CMPS11_ADDR 0x60		//default value returned.
+
+#define COMMAND_REG 0 			//read for software version, write to issue a command.
+#define COMPASS_8 1 			//8 bit 0-255 for full circle
+#define COMPASS_16_HIGH 2		//16 bit full 360 degrees from the compass, this is the HIGH byte
+#define COMPASS_16_LOW 3		//16 bit full 360 degrees from the compass, this is the LOW byte
+#define PITCH_ANGLE 4			// 8 bit pitch angle with kaman filter (prevents reaction to shaking)
+#define ROLL_ANGLE 5			// 8 bit roll angle with kaman filter (prevents reaction to shaking)
+#define MAGNETOMETER_X_RAW_HIGH 6	// high byte compass raw data for specific Axis
+#define MAGNETOMETER_X_RAW_LOW 7	// high byte compass raw data for specific Axis
+#define MAGNETOMETER_Y_RAW_HIGH 8	// high byte compass raw data for specific Axis
+#define MAGNETOMETER_Y_RAW_LOW 9	// high byte compass raw data for specific Axis
+#define MAGNETOMETER_Z_RAW_HIGH 10 	// high byte compass raw data for specific Axis
+#define MAGNETOMETER_Z_RAW_LOW 11	// high byte compass raw data for specific Axis
+#define ACCELEROMETER_X_RAW_HIGH 12	// high byte compass raw data for specific Axis	
+#define ACCELEROMETER_X_RAW_LOW 13	// high byte compass raw data for specific Axis
+#define ACCELEROMETER_Y_RAW_HIGH 14	// high byte compass raw data for specific Axis
+#define ACCELEROMETER_Y_RAW_LOW 15	// high byte compass raw data for specific Axis
+#define ACCELEROMETER_Z_RAW_HIGH 16	// high byte compass raw data for specific Axis
+#define ACCELEROMETER_Z_RAW_LOW 17	// high byte compass raw data for specific Axis
+#define GYRO_X_RAW_HIGH 18		// high byte compass raw data for specific Axis
+#define GYRO_X_RAW_LOW 19		// high byte compass raw data for specific Axis
+#define GYRO_Y_RAW_HIGH 20		// high byte compass raw data for specific Axis
+#define GYRO_Y_RAW_LOW 21		// high byte compass raw data for specific Axis
+#define GYRO_Z_RAW_HIGH 22		// high byte compass raw data for specific Axis
+#define GYRO_Z_RAW_LOW 23		// high byte compass raw data for specific Axis
+#define TEMP_RAW_HIGH 24		// high byte temperature data
+#define TEMP_RAW_LOW 25			// low byte temperature data 
+#define PITCH_ANGLE_NO_FILTER 26	// pitch angle - no filter 8 bit
+#define ROLL_ANGLE_NO_FILTER 27		// roll angle - no filter 8 bit
+
+struct m_coord;
+{
+	short x,y,z;
+};
+
+class Orientation : public I2C
+{
+private:
+	int8 m_pitch;
+	int8 m_roll;	
+	int8 m_pitch_nofilt;
+	int8 m_roll_nofilt;
+	short m_compassBearing;
+	short m_Temperature;	
+	struct m_coord m_magnometer, m_gyro, m_accelerometer;
+	
+	short convertShort(int8 high, int8 low);	
+public :
+	void showSoftVersion();
+	int calibrateDevice();
+	int restoreFactoryDefault();	
+	short m_getBearing16(); 
+	short m_getBearing8();
+	struct m_coord getMagnometer();
+	struct m_coord getGyro();
+	struct m_coord getAccelerometer();
+	int8 getPitch(bool mode);		// true=filter on, false=filter off
+	int8 getRoll(bool mode);
+	Orientation();
+	~Orientation();
+};
+
+#endif
